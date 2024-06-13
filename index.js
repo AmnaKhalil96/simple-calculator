bef2d1fc497f9b97701b065d9fc114685c0e2855
AmnaKhalil96 <anna963621@gmail.com>
2024-06-14 02:45:09 +0500

    second commit

--- index.js
+++ index.js
@@ -1,31 +1,60 @@
 #! /usr/bin/env node
 import inquirer from "inquirer";
+import chalk from "chalk";
 //Printing a Welcome Message
-console.log("\n\tWelcome To \'Amna Khalil\' - CLI Simple Calculator\n");
-//Asking Question from Users through Inquirer
-const answer = await inquirer.prompt([
-    { message: "Enter first number", type: "number", name: "firstNumber" },
-    { message: "Enter second number", type: "number", name: "secondNumber" },
-    {
-        message: "Select one of the operators to perform operation",
-        type: "list",
-        name: "operator",
-        choices: ["Addition", "Subtraction", "Multiplication", "Division"],
-    },
-]);
-//Conditional statements If-Else
-if (answer.operator === "Addition") {
-    console.log(answer.firstNumber + answer.secondNumber);
+console.log(chalk.bold.rgb(204, 204, 204)(`\n  \t\t <<<================================================>>>`));
+console.log(chalk.bold.rgb(204, 204, 204)(`<<<=========>>>  ${chalk.redBright.bold('Welcome To \"SAM\" - CLI Simple Calculator')}  <<<=========>>>`));
+console.log(chalk.bold.rgb(204, 204, 204)(`\t\t <<<===============================================>>>\n`));
+// Function to run the calculator
+async function main() {
+    let runCalculator = true;
+    while (runCalculator) {
+        // Asking Questions from Users through Inquirer
+        const answer = await inquirer.prompt([
+            { message: (chalk.blueBright.bold("Enter first number:")), type: "number", name: "firstNumber", validate: input => !isNaN(input) || chalk.red("Please enter a valid number") },
+            { message: (chalk.blueBright.bold("Enter second number:")), type: "number", name: "secondNumber", validate: input => !isNaN(input) || chalk.red("Please enter a valid number") },
+            {
+                message: (chalk.yellowBright.bold.italic("Select one of the operators to perform operation:")),
+                type: "list",
+                name: "operator",
+                choices: ["Addition", "Subtraction", "Multiplication", "Division", "Exit"],
+            },
+        ]);
+        //Check if the user wants to exit
+        if (answer.operator === "Exit") {
+            console.log(chalk.magentaBright("Exiting the calculator. Thank you for using it!"));
+            return; // Exit the function
+        }
+        //Conditional statements If-Else
+        if (answer.operator === "Addition") {
+            console.log(chalk.green(answer.firstNumber + answer.secondNumber));
+        }
+        else if (answer.operator === "Subtraction") {
+            console.log(chalk.green(answer.firstNumber - answer.secondNumber));
+        }
+        else if (answer.operator === "Multiplication") {
+            console.log(chalk.green(answer.firstNumber * answer.secondNumber));
+        }
+        else if (answer.operator === "Division") {
+            console.log(chalk.green(answer.firstNumber / answer.secondNumber));
+        }
+        else {
+            console.log(chalk.redBright("Please select valid operator"));
+        }
+        ////
+        const { confirm } = await inquirer.prompt({
+            type: "confirm",
+            name: "confirm",
+            message: chalk.magentaBright("Do you want to perform another calculation?"),
+            default: false,
+        });
+        runCalculator = confirm;
+    }
+    console.log(chalk.yellow("__________________________________________________________________________"));
+    console.log(chalk.greenBright("\n\tThank you for using the SAM Calculator!\t"));
+    // Recursively call the function to keep the calculator running
+    //  await runCalculator();
+    // // Call the function to start the calculator
+    //  runCalculator()};
 }
-else if (answer.operator === "Subtraction") {
-    console.log(answer.firstNumber - answer.secondNumber);
-}
-else if (answer.operator === "Multiplication") {
-    console.log(answer.firstNumber * answer.secondNumber);
-}
-else if (answer.operator === "Division") {
-    console.log(answer.firstNumber / answer.secondNumber);
-}
-else {
-    console.log("Please select valid operator");
-}
+main();
